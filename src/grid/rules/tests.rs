cda614eaa0ca1f1180d9764015349c9a6cceba5e
Hall Liu <hallliu000@gmail.com>
2016-07-03 21:55:53 -0700

    wrote out more tests

--- src/grid/rules/tests.rs
+++ src/grid/rules/tests.rs
@@ -9,7 +9,7 @@ static TEST_GRID_STR: &'static str =
 "____0
  X1__X
  XX__X
- 0__21
+ 1__21
  _X___";
 
 #[test]
@@ -18,12 +18,11 @@ fn test_zero_rule() {
     "___^0
      X1__X
      XX__X
-     0^_21
-     ^X___".replace(char::is_whitespace, "");
+     1__21
+     _X___".replace(char::is_whitespace, "");
     let mut test_grid = precompute_data(get_grid_from_string(TEST_GRID_STR, 5).unwrap());
 
     apply_constraint_rule(&mut test_grid, 4);
-    apply_constraint_rule(&mut test_grid, 15);
 
     assert_eq!(&expected_result, &print_grid_to_string(&test_grid.grid, false));
 }
@@ -34,7 +33,7 @@ fn test_number_light_rule() {
     "____0
      X1__X
      XX__X
-     0__21
+     1__21
      _X##*".replace(char::is_whitespace, "");
 
     let mut test_grid = precompute_data(get_grid_from_string(TEST_GRID_STR, 5).unwrap());
@@ -49,13 +48,14 @@ fn test_number_light_rule_multi() {
     "#*##0
      X1##X
      XX#*X
-     0#*21
-     _X##*".replace(char::is_whitespace, "");
+     1#*21
+     *X##*".replace(char::is_whitespace, "");
 
     let mut test_grid = precompute_data(get_grid_from_string(TEST_GRID_STR, 5).unwrap());
     apply_constraint_rule(&mut test_grid, 19);
     apply_constraint_rule(&mut test_grid, 18);
     apply_constraint_rule(&mut test_grid, 6);
+    apply_constraint_rule(&mut test_grid, 15);
 
     assert_eq!(&expected_result, &print_grid_to_string(&test_grid.grid, false));
 }
@@ -66,7 +66,7 @@ fn test_number_corner_rule() {
     "__^_0
      X1__X
      XX^_X
-     0__21
+     1__21
      _X^__".replace(char::is_whitespace, "");
 
     let mut test_grid = precompute_data(get_grid_from_string(TEST_GRID_STR, 5).unwrap());
