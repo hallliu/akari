1fd52295bb0899c5980cbb491d8f283d8cad7472
Hall Liu <hallliu000@gmail.com>
2016-07-19 00:40:23 -0700

    more generator stuff

--- puzzle_generator.py
+++ puzzle_generator.py
@@ -4,12 +4,38 @@ Puzzle generation routines for Akari.
 
 import random
 
+class Grid():
+    height = -1
+    width = -1
+    squares = None
+
+    def __init__(self, height, width, density):
+        self.height = height
+        self.width = width
+        self.squares = {}
+        for v in range(height):
+            for h in range(width):
+                self.squares[(v, h)] = GridSquare((v, h))
+
+        for square in self.squares.values():
+            square.set_neighbors(self.squares)
+            if density > random.random():
+                square.is_solid = True
+
+    def __str__(self):
+        return '\n'.join(
+            ''.join(str(self.squares[(v, h)]) for h in range(self.width))
+            for v in range(self.height)
+        )
+
 class GridSquare():
-    location = None
+    location = (-1, -1)
     is_lit = False
     is_solid = False
     is_light = False
-    number_constraint = None
+    has_number_constraint = False
+
+    num_surrounding_lights = -1 
 
     # Ordered starting at the top going clockwise.
     neighbors = [None, None, None, None]
@@ -18,10 +44,10 @@ class GridSquare():
         self.location = location
 
     def set_neighbors(self, loc_table):
-        self.neighbors[0] = loc_table[(self.location[0] - 1, self.location[1])]
-        self.neighbors[1] = loc_table[(self.location[0], self.location[1] + 1)]
-        self.neighbors[2] = loc_table[(self.location[0] + 1, self.location[1])]
-        self.neighbors[3] = loc_table[(self.location[0], self.location[1] - 1)]
+        self.neighbors[0] = loc_table.get((self.location[0] - 1, self.location[1]))
+        self.neighbors[1] = loc_table.get((self.location[0], self.location[1] + 1))
+        self.neighbors[2] = loc_table.get((self.location[0] + 1, self.location[1]))
+        self.neighbors[3] = loc_table.get((self.location[0], self.location[1] - 1))
 
     """
     Assumes that the neighbors have been initialized.
@@ -37,39 +63,32 @@ class GridSquare():
                 yield curr_square
                 curr_square = curr_square.get_neighbor(nbr_dir)
 
-    def get_num_surrounding_lights(self):
-        result = 0
-        for neighbor in self.neighbors:
-            if neighbor is not None and neighbor.is_light:
-                result += 1
-
-        return result
+    def set_num_surrounding_lights(self):
+        self.num_surrounding_lights = self._count_neighbors(lambda n: n.is_light)
 
     def get_neighbor(self, direction):
         return self.neighbors[direction]
 
+    def get_num_surrounding_nonsolid_squares():
+        return self._count_neighbors(lambda n: not n.is_solid)
+
+    def _count_neighbors(self, matches):
+        result = 0
+        for neighbor in self.neighbors:
+            if neighbor is not None and matches(neighbor):
+                result += 1
+        return result
+
     def __str__(self):
-        if self.number_constraint is not None:
-            return str(self.number_constraint)
+        if self.has_number_constraint:
+            return str(self.num_surrounding_lights)
         if self.is_solid:
             return "X"
         return "_"
 
+def main():
+    g1 = Grid(7, 7, 0.3)
+    print(str(g1))
 
-def make_empty_grid(height, width):
-    table = {}
-    for v, h in zip(range(height), range(width)):
-        table[(v, h)] = GridSquare((v, h))
-
-    for square in table.values():
-        square.set_neighbors(table)
-
-    return table
-
-def make_unlit_grid(height, width, density):
-    table = make_empty_grid(height, width)
-    for square in table.values():
-        if density > random.random():
-            square.is_solid = True
-
-    return table
+if __name__ == "__main__":
+    main()
